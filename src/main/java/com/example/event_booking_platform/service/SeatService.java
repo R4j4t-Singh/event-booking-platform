3c66da8b2b3cf7d80f37b02f04e0fc8dd712f8f4
R4j4t-Singh <202011061@diu.iiitvadodara.ac.in>
2025-04-28 19:49:22 +0530

    Added exception for show not found

--- src/main/java/com/example/event_booking_platform/service/SeatService.java
+++ src/main/java/com/example/event_booking_platform/service/SeatService.java
@@ -3,6 +3,7 @@ package com.example.event_booking_platform.service;
 import com.example.event_booking_platform.entity.Seat;
 import com.example.event_booking_platform.entity.Show;
 import com.example.event_booking_platform.exception.EventNotFoundException;
+import com.example.event_booking_platform.exception.ShowNotFoundException;
 import com.example.event_booking_platform.repository.SeatRepository;
 import com.example.event_booking_platform.repository.ShowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class SeatService {
     @Autowired
     private ShowRepository showRepository;
 
-    public List<Seat> getSeats(Long showId) throws EventNotFoundException {
-        Show show = showRepository.findById(showId).orElseThrow(() -> new EventNotFoundException("change it"));
+    public List<Seat> getSeats(Long showId) throws ShowNotFoundException {
+        Show show = showRepository.findById(showId).orElseThrow(() -> new ShowNotFoundException("Show not found"));
         return seatRepository.findAllByShow(show);
     }
 }
